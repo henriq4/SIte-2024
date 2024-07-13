e23618e7a8d091640eb5beb73d01b0fa2e2555c6
Henrique GC <henriq4net@gmail.com>
2024-07-13 18:22:49 -0300

    feat: calendar page ui

--- lib/modules/calendar/pages/calendar_page.dart
+++ lib/modules/calendar/pages/calendar_page.dart
@@ -13,9 +13,71 @@ class _CalendarPageState extends State<CalendarPage> {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Calendário'),
+        centerTitle: true,
       ),
-      body: const Center(
-        child: Text('Calendário'),
+      body: const Column(
+        children: [
+          Padding(
+            padding: EdgeInsets.symmetric(
+              horizontal: 16.0,
+              vertical: 8.0,
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Calendário',
+                  style: TextStyle(
+                    fontSize: 20,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                Card.filled(
+                  child: SizedBox(
+                    width: double.infinity,
+                    height: 200,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(
+              horizontal: 16.0,
+              vertical: 8.0,
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Avalicações',
+                  style: TextStyle(
+                    fontSize: 20,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                Card.filled(
+                  child: SizedBox(
+                    width: double.infinity,
+                    height: 80,
+                  ),
+                ),
+                Card.filled(
+                  child: SizedBox(
+                    width: double.infinity,
+                    height: 80,
+                  ),
+                ),
+                Card.filled(
+                  child: SizedBox(
+                    width: double.infinity,
+                    height: 80,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
