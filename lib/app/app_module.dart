a684b0534481bba20036b921b7769be4cee706de
Henrique GC <henriq4net@gmail.com>
2024-09-01 12:38:57 -0300

    chore: adding shared module

--- lib/app/app_module.dart
+++ lib/app/app_module.dart
@@ -1,17 +1,19 @@
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:suap_uenp_app/guards/auth_guard.dart';
-import 'package:suap_uenp_app/modules/auth/auth_module.dart';
-import 'package:suap_uenp_app/modules/calendar/calendar_module.dart';
-import 'package:suap_uenp_app/modules/perfil/perfil_module.dart';
+import 'package:suap_uenp_app/modules/splash/splash_module.dart';
 
+import '../modules/auth/auth_module.dart';
+import '../modules/calendar/calendar_module.dart';
 import '../modules/core/core_module.dart';
 import '../modules/home/home_module.dart';
-import '../modules/splash/splash_module.dart';
+import '../modules/perfil/perfil_module.dart';
+import '../modules/shared/shared_module.dart';
 
 class AppModule extends Module {
   @override
   List<Module> get imports => [
         CoreModule(),
+        SharedModule(),
         AuthModule(),
         HomeModule(),
         PerfilModule(),
